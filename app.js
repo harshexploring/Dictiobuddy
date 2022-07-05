178b6d87329374156d678c992561f418c6e6159c
harshjha <harshjha22022002@gmail.com>
2022-07-05 20:27:07 +0530

    commit5

--- app.js
+++ app.js
@@ -262,6 +262,6 @@ app.post("/deletefromsavedwords", function (req, res) {
 
 
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+});
